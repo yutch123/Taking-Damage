0495c8e6213f0de5ebf551f45e4177aa98b9ca68
Dmitry <Ivanovdmitry530@gmail.com>
2023-06-30 17:01:30 +0400

    Добавьте файлы проекта.

---
+++ Taking Damage/Taking Damage.cpp
@@ -0,0 +1,24 @@
+﻿// Программа Taking Damage
+// Демонстрирует подстановку функций
+
+#include <iostream>
+int radiation(int health); // объявление функции
+using namespace std;
+
+int main()
+{
+	int health = 80;
+	cout << "Your health is " << health << "\n\n";
+	health = radiation(health);
+	cout << "After radiation exposure your health is " << health << "\n\n";
+	health = radiation(health);
+	cout << "After radiation exposure your health is " << health << "\n\n";
+	health = radiation(health);
+	cout << "After radiation exposure your health is " << health << "\n\n";
+	return 0;
+}
+inline int radiation(int health)
+{
+	return (health / 2);
+}
+
